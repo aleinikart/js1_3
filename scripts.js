65edce26b52a2af95c79c510f562db8a512f0cc3
aleynik <fadeghost@gmail.com>
2018-12-29 02:08:40 +0300

    dz3

---
+++ scripts.js
@@ -0,0 +1,46 @@
+/*п. 1*/
+
+// var arr, j, i = 0;
+//
+// while (i < 100){
+//    j = 1;
+//    arr = [];
+//    while (j <= 9){
+//       if (i%j == 0 && j != 1 && j != i){
+//         arr.push(j);
+//       }
+//       j++;
+//    }
+//    if (arr.length == 0){
+//      console.log(i);
+//    }
+//    i++;
+// }
+
+/*п. 2*/
+// var i = 0;
+// do{
+//   if (i == 0) {
+//     console.log(i + '– это ноль');
+//   } else {
+//     if (i%2 == 0){
+//       console.log(i + '– это четное число');
+//     } else {
+//       console.log(i + '– это нечетное число');
+//     }
+//   }
+//   i++;
+// } while (i<=10);
+
+/*п. 3*/
+
+// for (var i = 0; i <= 9; i++){
+//   console.log(i);
+// }
+
+/*п. 4*/
+// var k = '', j = 'x';
+// for(var i = 0; i < 20; i++){
+//   k += j;
+//   console.log(k);
+// }
